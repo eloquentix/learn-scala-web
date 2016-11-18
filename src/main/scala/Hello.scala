bda0a18887871ff005766b453d3fec3294cde6ef
Andrei Oprisan <oprisan@eloquentix.com>
2016-11-18 11:38:51 +0200

    Added Using

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,26 @@
-object Hello {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+
+case class Using[T <: {def close(): Unit}](resource: T) {
+  def apply[U](block: T => U): U = {
+    try {
+      block(resource)
+    } finally {
+      resource.close()
+    }
   }
 }
+
+case class DbConnection(url: String) {
+  def close(): Unit = ()
+  def runQuery(s: String): String = ""
+}
+
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    Using(DbConnection("url")) { conn =>
+      conn.runQuery("query")
+    }
+  }
+}
+
+
